326e783f8721b27f9d659c5202111801b200633d
fslopes <fslopes@users.noreply.github.com>
2014-11-23 06:02:16 +0000

    Added aliases

--- query_offices.sql
+++ query_offices.sql
@@ -1,4 +1,4 @@
-SELECT offices.id, rooms.room, teachers.name
+SELECT offices.id AS Office, rooms.room AS Room, teachers.name AS Teacher
 	FROM offices, rooms, teachers
 	WHERE offices.roomID = rooms.id
 		AND offices.teacherID = teachers.id
